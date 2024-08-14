53c2121514937fcccc41594587cad853ba997cbb
Thrishams <thrisha2003mani@gmail.com>
2024-08-14 15:40:20 +0530

    Add files via upload

---
+++ C43_Thrisha/src/com/tnsif/day1/program/program4.java
@@ -0,0 +1,13 @@
+package com.tnsif.day1.program;
+
+public class program4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+		{	
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+	}
+}
